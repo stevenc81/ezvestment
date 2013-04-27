8b26eb26232d311a9867c4afda3960303ce11282
Steven Cheng <stevenc81@gmail.com>
2013-04-27 21:58:36 +0800

    - added slider with jquery ui

--- webapp/js/ezvestment.js
+++ webapp/js/ezvestment.js
@@ -1,11 +1,14 @@
-var app = angular.module('ezvestment', ['$strap.directives']).
+var app = angular.module('ezvestment', ['$strap.directives', 'ezvestment-directives']).
     config(function ($routeProvider) {
         $routeProvider.
             when('/', {controller: AllocCtrl, templateUrl: 'alloc.html'}).
             otherwise({redirectTo: '/'});
     });
 
+function HeaderCtrl($scope) {
+    console.log('# In HeaderCtrl');
+}
+
 function AllocCtrl($scope) {
-    console.log('# In IndexCtrl');
-    $scope.project_name = 'Ezvestment';
+    console.log('# In AllocCtrl');
 }
---
+++ webapp/js/ezvestment_directives.js
@@ -0,0 +1,19 @@
+var app = angular.module('ezvestment-directives', []);
+
+app.directive('slider', function() {
+    console.log('# In slider directive');
+    return {
+        link: function(scope, element, attrs) {
+            element.slider({
+                range: true,
+                min: 0,
+                max: 100,
+                values: 50,
+                slide: function(event, ui) {
+                    console.log(ui.value);
+                }
+            });
+        }
+    };
+});
+
