c59b01fb33e94b054160ae6430b6a4c8a4fba2b1
Steven Cheng <stevenc81@gmail.com>
2013-05-05 21:26:03 +0800

    - actuated slider and return graph

--- webapp/js/ezvestment_directives.js
+++ webapp/js/ezvestment_directives.js
@@ -3,7 +3,7 @@ var app = angular.module('ezvestment-directives', []);
 app.directive('slider', function() {
     console.log('# In slider directive');
     return {
-        controller: AllocCtrl,
+        restrict: 'A',
         link: function(scope, element, attrs) {
             element.slider({
                 range: true,
@@ -11,10 +11,20 @@ app.directive('slider', function() {
                 max: 100,
                 values: 50,
                 slide: function(event, ui) {
-                    console.log(ui.value);
-                    scope.slider_value = ui.value;
                     scope.portionBounds = 100 - ui.value;
                     scope.portionStocks = ui.value;
+
+                    var newData = [];
+                    for (i = 1; i < 10; i++) {
+                        x = i;
+                        y = scope.portionStocks*i/10;
+                        newData.push([x, y]);
+                    }
+
+                    scope.plot1.series[0].data = newData;
+                    // scope.plot1.resetAxesScale();
+                    scope.plot1.replot();
+
                     scope.$apply();
                 }
             });
@@ -26,7 +36,7 @@ app.directive('tooltip', function() {
     console.log('# In Tooltip directive');
     return {
         restrict:'A',
-        require: 'slider',
+        // require: 'slider',
         link: function(scope, element, attrs) {
             element.tooltip({
                 placement: 'top',
@@ -39,12 +49,32 @@ app.directive('tooltip', function() {
 app.directive('chart', function() {
    console.log('# In chart directive');
     return {
-        restrict: 'A',
         link: function(scope, element, attrs) {
             element.ready(function(){
-                var plot1 = $.jqplot ('chart1', [[3,7,9,1,5,3,8,2,5]], {
-                    title: 'Main Chart'
-                });
+                var plot1 = $.jqplot ('chart1', [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], {
+                        title: 'Main Chart',
+                        axesDefaults: {
+                            labelRenderer: $.jqplot.CanvasAxisLabelRenderer
+                        },
+                        series: [{
+                            showMarker: false
+                        }],
+                        axes: {
+                            xaxis: {
+                                min: 0,
+                                max: 10,
+                                numberTicks: 11,
+                                label: '% of Return'
+                            },
+                            yaxis : {
+                                min: 0,
+                                max: 100,
+                                label: 'Year'
+                            }
+                        }
+                    });
+
+                scope.plot1 = plot1;
             });
         }
     };
