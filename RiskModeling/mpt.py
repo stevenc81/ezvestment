7161316cdff86c6c688950abbdc45fad24aa846d
Steven Cheng <stevenc81@gmail.com>
2013-06-14 21:03:44 +0800

    - First attempt to implement port opt

--- RiskModeling/mpt.py
+++ RiskModeling/mpt.py
@@ -3,20 +3,18 @@ import math
 import numpy as np
 
 #
-# Constants
+# Constants (finicial magic numbers)
 #
 # yield from 3 months treasury
 RFR = 0.05
-
 # 2012 yeild for S&P 500
 ERM = 13.4
-
+# Ticker for benchmark
 BENCH_TICKER = '^GSPC'
-
 dataPath = 'data/'
 
 
-class StockModel():
+class StockModel(object):
     def __init__(self, ticker, ticker_history, bench_history, rfr, erm):
         self.ticker = ticker
         self.ticker_history = np.array(ticker_history)
@@ -65,9 +63,9 @@ class StockModel():
         return self.rfr + beta * (self.erm - self.rfr)
 
     def _calculate_performance(self):
-        daily_returns = self._calculate_return()
-        self.daily_volatility = daily_returns.std()
-        self.beta = self._calculate_beta(daily_returns)
+        self.daily_returns = self._calculate_return()
+        self.daily_volatility = self.daily_returns.std()
+        self.beta = self._calculate_beta(self.daily_returns)
 
         self.expected_return = self._calculate_expected_return(self.beta)
 
@@ -90,7 +88,7 @@ class StockModel():
         return (cutArray, smallArray)
 
 
-class PortfolioModel():
+class PortfolioModel(object):
     def __init__(self, tickers, bench_ticker, rfr, erm):
         getdata.get_ticker_history(tickers)
         getdata.get_ticker_history([bench_ticker])
@@ -104,7 +102,9 @@ class PortfolioModel():
                 lines[row].strip().split(',')
             bench_history.append(float(adjClose))
 
-        self.stocks = []
+        self.stocks = {}
+        self.mix = {}
+        self.tickers = tickers
         for ticker in tickers:
             ticker_history = []
             with open('data/' + ticker + '.csv', 'r') as f:
@@ -115,27 +115,138 @@ class PortfolioModel():
                     lines[row].strip().split(',')
                 ticker_history.append(float(adjClose))
 
-            self.stocks.append(
-                StockModel(
-                    ticker,
-                    ticker_history,
-                    bench_history,
-                    rfr,
-                    erm))
+            self.stocks[ticker] = StockModel(
+                ticker, ticker_history,
+                bench_history,
+                rfr,
+                erm)
+
+            self.mix[ticker] = 1 / len(tickers)
 
     def get_stocks(self):
         return self.stocks
+
+    def evaluate_holdings(self):
+        """ This method updates the return for the portfolio
+            overall, based on the weights and returns of the components
+            in the portfolio.  It returns a tuple of (variance,
+            portfolio_expected_return)
+        """
+
+        port_return = 0.0
+
+        for ticker in self.tickers:
+            port_return += (self.stocks[ticker].expected_return * self.mix[ticker])
+
+        self.portfolio_return = port_return
+
+        # variance = self.calc_variance()
+        return port_return
+
+    def calculate_marginal_utility(self, rt=0.20):
+        ''' using capm def of exoected return might be in correct in sharp's
+        gradiant method '''
+        e_ary = np.array([self.stocks[ticker].expected_return
+                         for ticker in self.tickers])
+
+        e = np.mat(e_ary).T
+
+        ratelist = []
+
+        for ticker in self.tickers:
+            ratelist.append(self.stocks[ticker].daily_returns.tolist())
+
+        rates = np.array(ratelist, dtype=float)
+
+        cv = np.cov(rates)
+        C = np.mat(cv)
+
+        mix = np.array([self.mix[ticker]
+                        for ticker in self.tickers])
+        x = np.mat(mix).T
+
+        mu = e - (1/rt)*2*C*x
+
+        return mu
+
+    def step_port_return(self, rt=0.20, lower_bound_weight=-2.0,
+                         upper_bound_weight=2.0):
+
+        mix = np.array([self.mix[ticker]
+                        for ticker in self.tickers])
+        x = np.mat(mix).T
+
+        mu = self.calculate_marginal_utility(rt)
+
+        mubuy = -1E200
+        musell = 1E200
+        ibuy = 0
+        isell = 0
+
+        for i in range(len(self.tickers)):
+            if x[i] < upper_bound_weight: # possible buy
+                if mu[i,0] > mubuy:
+                    mubuy = mu[i,0]
+                    ibuy = i
+
+            if x[i] > lower_bound_weight:  # possible sell
+                if mu[i,0] < musell:
+                    musell = mu[i,0]
+                    isell = i
+
+        if (mubuy-musell)<=0.0001:
+            ibuy=isell=0
+
+        s = np.zeros(mu.shape)
+        s[isell]=-1.0
+        s[ibuy]=1.0
+        s = np.mat(s)
+
+        rates = np.array([self.stocks[ticker].daily_returns
+            for ticker in self.tickers])
+        C = np.mat(np.cov(rates))
+
+        k0 = s.T*mu
+        k1 = (s.T*C*s)/rt
+
+        amat = k0/(2*k1)
+        a = amat[0,0]
+
+        # a change of weights according to 'a' will yield a utility
+        # change as indicated below.  This is not currently returned or used anywhere,
+        # but it's calculated here for potential future use.
+        cu = k0*a - k1*(a**2)
+
+        symb_sell = self.tickers[isell]
+        symb_buy = self.tickers[ibuy]
+        if symb_sell == symb_buy:
+            a = 0.0
+
+        # Keep within the bounds
+        if self.mix[symb_sell] - a < lower_bound_weight:
+            a = self.mix[symb_sell] - lower_bound_weight
+        if self.mix[symb_buy] + a > upper_bound_weight:
+            a = upper_bound_weight - self.mix[symb_buy]
+
+        self.mix[symb_sell] = self.mix[symb_sell] - a
+        self.mix[symb_buy] = self.mix[symb_buy] + a
+
+        #print("Recommend Sell: %s" % symb_sell)
+        #print("Recommend Buy: %s" % symb_buy)
+        #print("Affect on Portfolio Return: %s" % cu)
+
+        return a
 
 
 if __name__ == "__main__":
 
     # get_ticker_history(['CHIQ', 'FSG', 'GLD', 'GMF', 'IPK', 'TAO', 'VGK', 'VPL', 'VWO'])
-    portfolio = PortfolioModel(getdata.scrape_all_ishare_etfs(), BENCH_TICKER, RFR, ERM)
+    portfolio = PortfolioModel(getdata.scrape_free_etfs(), BENCH_TICKER, RFR, ERM)
 
-    for stock in portfolio.get_stocks():
+    for ticker, stock in portfolio.get_stocks().iteritems():
         print "".join([
             "Ticker: ",
-            stock.get_ticker(),
+            ticker,
             " Volatility: ", str(stock.get_daily_volatility()),
             " Beta: ", str(stock.get_beta()),
             " Return: ", str(stock.get_expected_return())])
@@ -145,3 +256,25 @@ if __name__ == "__main__":
                 stock.get_ticker(), ',',
                 str(stock.get_daily_volatility()), ',',
                 str(stock.get_expected_return()), '\n']))
+
+    rt = 0.1
+    lower_bound_weight = -0.50
+    upper_bound_weight = 1.5
+    a = 1.0
+    count = 0
+
+    while a>0.00001:
+        a = portfolio.step_port_return(rt, lower_bound_weight,
+                                  upper_bound_weight)
+
+        count+=1
+
+    result = portfolio.evaluate_holdings()
+    # variance = round(result[0],3)
+    ret = round(result*100.,3)
+
+    print("Optimization completed in [ %s ] iterations." % count)
+    print("Ending weights:\n%s" % portfolio.mix)
+    # print("Volatility: %s and Portfolio Return: %s%%" % (np.sqrt(variance), ret))
+    # opt_rate_array = portfolio.calc_port_rates()
+    # print("Portfolio Rate Array:%s\n" % opt_rate_array[:10])
